b56b73ef6dd512687d760ae4321f2ffec7830bb6
Fabian Benavides <didierfabianbenavidesrojas@gmail.com>
2025-11-07 14:10:30 -0500

    [FB][HU-0] Commit inicial

---
+++ src/main/java/com/huipa/huipa/HuipaApplication.java
@@ -0,0 +1,13 @@
+package com.huipa.huipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuipaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuipaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huipa/huipa/HuipaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huipa.huipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuipaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
