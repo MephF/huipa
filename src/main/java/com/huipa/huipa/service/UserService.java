0ed61fc7f59b58ee97f712d732989d9a37ac2803
Fabian Benavides <didierfabianbenavidesrojas@gmail.com>
2025-11-07 15:37:59 -0500

    [FB][HU-02] Login de CLIENTES y ARTESANOS

--- src/main/java/com/huipa/huipa/service/UserService.java
+++ src/main/java/com/huipa/huipa/service/UserService.java
@@ -1,8 +1,10 @@
 package com.huipa.huipa.service;
 
+import com.huipa.huipa.dtos.UserLoginDto; // Import UserLoginDto
 import com.huipa.huipa.dtos.UserRegistrationDto;
 import com.huipa.huipa.entity.User;
 
 public interface UserService {
     User registerUser(UserRegistrationDto registrationDto);
+    User loginUser(UserLoginDto loginDto); // Added loginUser method
 }
